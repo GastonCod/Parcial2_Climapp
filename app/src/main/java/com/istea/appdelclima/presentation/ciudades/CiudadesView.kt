1924352cd43aae1557358035c3b9991250880b28
Gaston Ferreiro <gastonagustinferreiro@gmail.com>
2025-11-16 19:25:02 -0300

    quito import

--- app/src/main/java/com/istea/appdelclima/presentation/ciudades/CiudadesView.kt
+++ app/src/main/java/com/istea/appdelclima/presentation/ciudades/CiudadesView.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.istea.appdelclima.repository.modelos.Ciudad
